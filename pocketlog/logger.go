e146df5df4ee25a4093d4df6e2170c976db33378
bmccarson <brian@mccarson.us>
2024-05-09 17:09:23 -0400

    add threshold to logger struct. create New() init method for logger.

--- pocketlog/logger.go
+++ pocketlog/logger.go
@@ -2,4 +2,12 @@ package pocketlog
 
 // Logger is used to log information.
 type Logger struct {
+	threshold Level
+}
+
+// New returns you a logger, ready to log at the required threshold.
+func New(threshold Level) *Logger {
+	return &Logger{
+		threshold: threshold,
+	}
 }

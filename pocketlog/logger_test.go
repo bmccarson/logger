abad072c4a95eb76cddd2b3effaffb0f38e094d8
bmccarson <brian@mccarson.us>
2024-05-11 13:36:35 -0400

    update test for initl options impl on New()

--- pocketlog/logger_test.go
+++ pocketlog/logger_test.go
@@ -1,13 +1,11 @@
 package pocketlog_test
 
 import (
-	"os"
-
 	"github.com/bmccarson/logger/pocketlog"
 )
 
 func ExampleLogger_Debugf() {
-	debugLogger := pocketlog.New(pocketlog.LevelDebug, os.Stdout)
+	debugLogger := pocketlog.New(pocketlog.LevelDebug)
 	debugLogger.Debugf("Hello, %s", "world")
 	// Output: Hello, world
 }

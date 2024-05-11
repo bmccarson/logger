880699390572a1b1483c48dc190533a661e6e0df
bmccarson <brian@mccarson.us>
2024-05-11 13:39:39 -0400

    basic test of Logger functions in main()

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"os"
+	"time"
+
+	pl "github.com/bmccarson/logger/pocketlog"
+)
+
 func main() {
+
+	lgr := pl.New(pl.LevelInfo, pl.WithOutput(os.Stdout))
+
+	lgr.Infof("A little copying is better than a little dependency.")
+	lgr.Errorf("Errors are values. Documentation is for %s.", "users")
+	lgr.Debugf("Make the zero (%d) value useful.", 0)
+
+	lgr.Infof("Hallo, %d %v", 2022, time.Now())
 
 }
